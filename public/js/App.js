f4fb8f6a58bfc9e91aa805ebb5adcb17ce28b1c8
Robin Kumar <robinkumar1422@gmail.com>
2021-05-26 21:01:05 +0530

    Deleted Unused Files

---
+++ public/js/App.js
@@ -0,0 +1,31 @@
+console.log('Client side JavaScript page is loaded!!!')
+const weatherForm=document.querySelector('form')
+const searchElement=document.querySelector('input')
+const forecast=document.querySelector('#Forecast')
+const locationmsg=document.querySelector('#location')
+const addressMsg=document.querySelector('#address')
+weatherForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const location=searchElement.value;
+    console.log(location);
+    fetch('./weather?address='+location).then((response)=>{
+        response.json().then((data)=>{
+            
+            addressMsg.textContent='Loading...'
+            if(data.error)
+            {
+                addressMsg.textContent=data.error
+            }
+            else
+            {
+                forecast.textContent=data.forecast
+                locationmsg.textContent=data.location
+                addressMsg.textContent=data.address
+            console.log(data.location)
+            console.log(data.forecast)
+            console.log(data.address)
+            }
+        })
+        })
+
+})
